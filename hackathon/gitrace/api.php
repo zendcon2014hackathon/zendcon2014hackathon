51e8d0333ec4dfb2899b2ef48149d7ba42940166
wimg <wim@wimgodden.be>
2014-10-29 14:44:36 -0700

    Changing to Joind.in

--- hackathon/gitrace/api.php
+++ hackathon/gitrace/api.php
@@ -1,94 +1,108 @@
 <?php
 
-switch ($_GET['action']) {
-    case 'getproject':
-        $c = curl_init('https://api.github.com/search/repositories?access_token=2e90abf397abb4f2df31451adcff5427f868d45f&q=' . $_GET['q']);
-        curl_setopt($c, CURLOPT_USERAGENT, 'Firefox');
-        curl_setopt($c, CURLOPT_SSL_VERIFYHOST, false);
-        curl_setopt($c, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
-        $result = curl_exec($c);
-        if ($result == false) {
-            echo 'Error';
-            break;
-        }
-        $result = json_decode($result);
-        $returnArray = array();
-        foreach ($result->items as $item) {
-            $returnArray[] = $item->full_name;
-        }
-        sort($returnArray, SORT_STRING | SORT_FLAG_CASE);
-        echo json_encode($returnArray);
-        break;
-    case 'getcommits':
-        $c = curl_init('https://api.github.com/repos/' . $_GET['project1'] . '/commits?access_token=2e90abf397abb4f2df31451adcff5427f868d45f');
-        curl_setopt($c, CURLOPT_USERAGENT, 'Firefox');
-        curl_setopt($c, CURLOPT_SSL_VERIFYHOST, false);
-        curl_setopt($c, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
-        $project1result = curl_exec($c);
-        if ($project1result == false) {
-            echo 'Error';
-            break;
-        }
-        $project1result = json_decode($project1result);
-        if (is_null($project1result[0]->author)) {
-            echo json_encode(
-                array(
-                    'error' => 'No valid commits'
-                )
-            );
-            break;
-        }
-        
-        $c = curl_init('https://api.github.com/repos/' . $_GET['project2'] . '/commits?access_token=2e90abf397abb4f2df31451adcff5427f868d45f');
-        curl_setopt($c, CURLOPT_USERAGENT, 'Firefox');
-        curl_setopt($c, CURLOPT_SSL_VERIFYHOST, false);
-        curl_setopt($c, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
-        $project2result = curl_exec($c);
-        if ($project2result == false) {
-            echo 'Error';
-            break;
-        }
-        $project2result = json_decode($project2result);
-        if (is_null($project2result[0]->author)) {
-            echo json_encode(
-                array(
-                    'error' => 'No valid commits'
-                )
-            );
-            break;
-        }
-        
-        $project1Return = array();
-        foreach ($project1result as $commit) {
-            $project1Return[] = array(
-                'user' => $commit->author->login,
-                'date' => $commit->commit->committer->date
-            );
-        }
-
-        foreach ($project2result as $commit) {
-            $project2Return[] = array(
-                'user' => $commit->author->login,
-                'date' => $commit->commit->committer->date
-            );
-        }
-        
-        echo json_encode(
-            array(
-                'project1' => $project1Return,
-                'project2' => $project2Return
-            )
-        );
-        
-        break;
+function callAPI($function, $params, $querystring = '') {
+    $paramString = '';
+    foreach ($params as $key=>$value) {
+        $paramString .= $key . '/' . $value . '/';
+    }
+    $ch = curl_init('http://api.joind.in/v2.1/' . $paramString . $function . '?format=json&resultsperpage=200' . ($querystring != '' ? '&' . $querystring : '')); 
+    curl_setopt($ch, CURLOPT_USERAGENT, 'ZendCon 2014 Hackathon');
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $result = curl_exec($ch);
+    if ($result == false) {
+        throw new Exception('Call to Joind.in API failed');
+    }
+    $result = json_decode($result);
+    return $result;    
 }
 
 
-// $c = curl_init('https://api.github.com/repos/zfcampus/zf-apigility/commits?access_token=2e90abf397abb4f2df31451adcff5427f868d45f');
-// curl_setopt($c, CURLOPT_USERAGENT, 'Firefox');
-// curl_setopt($c, CURLOPT_SSL_VERIFYHOST, false);
-// curl_setopt($c, CURLOPT_SSL_VERIFYPEER, false);
-// $result = curl_exec($c);
+switch ($_GET['action']) {
+    case 'getevents':
+        $events = callAPI('events', array(), 'filter=hot');
+        $returnArray = array();
+        foreach ($events->events as $event) {
+            $returnArray[] = array(
+                'id' => array_pop(explode('/', $event->uri)),
+                'name' => $event->name
+            );
+        }
+        echo json_encode($returnArray);
+        break;
+    case 'getspeakers':
+        $talks = callAPI('talks', array('events' => $_GET['id']));
+        $speakers = array();
+        foreach ($talks->talks as $talk) {
+            if (isset($talk->speakers) && isset($talk->speakers[0]) && isset($talk->speakers[0]->speaker_uri)) {
+                $speakers[array_pop(explode('/', $talk->speakers[0]->speaker_uri))] = $talk->speakers[0]->speaker_name;
+            }
+        }
+        
+        $returnArray = array();
+        foreach ($speakers as $key=>$value) {
+            $returnArray[] = array(
+                'id' => $key,
+                'name' => $value                  
+            );
+        }
+        echo json_encode($returnArray);
+        break;
+    case 'gettopratingsforevent':
+        $talks = callAPI('talks', array('events' => $_GET['id']));
+        $speakers = array();
+        $talklist = array();
+        foreach ($talks->talks as $talk) {
+            if (isset($talk->speakers) && isset($talk->speakers[0]) && isset($talk->speakers[0]->speaker_uri) && $talk->average_rating > 3 && $talk->comment_count > 1) {
+                $speakers[array_pop(explode('/', $talk->speakers[0]->speaker_uri))] = $talk->speakers[0]->speaker_name;
+                $talklist[array_pop(explode('/', $talk->speakers[0]->speaker_uri))][] = array_pop(explode('/', $talk->uri));
+            }
+        }
+
+        $returnArray = array();
+        foreach ($talklist as $id => $talks) {
+            $rating = 0;
+            $commentCount = 0;
+            $lowestRating = 5;
+            $highestRating = 0;
+            for ($cnt = 0; $cnt < count($talks); $cnt++) {
+                $comments = callAPI('comments', array('talks' => $talks[$cnt]));
+                foreach ($comments->comments as $comment) {
+                    if ($comment->rating > 0 && isset($comment->user_uri)) { // Speaker comments don't count
+                        $rating += $comment->rating;
+                        if ($comment->rating < $lowestRating) {
+                            $lowestRating = $comment->rating;
+                        }
+                        if ($comment->rating > $highestRating) {
+                            $highestRating = $comment->rating;
+                        }
+                        $commentCount++;
+                    }
+                }
+            };
+            
+            $returnArray[] = array(
+                'name' => $speakers[$id],
+                'avg' => $rating / $commentCount,
+                'lowest' => $lowestRating,
+                'highest' => $highestRating
+            );
+        }
+        
+        echo json_encode($returnArray);
+        break;
+    case 'getratings':
+        $talks = callAPI('talks', array('users' => $_GET['id']));
+        $returnArray = array();
+        foreach ($talks->talks as $talk) {
+            if ($talk->comment_count > 0 && $talk->average_rating > 0) { // Anonymous comments increase comment_count, but average_rating = 0 for those comments
+                $returnArray[] = array(
+                    'date' => $talk->start_date,
+                    'rating' => $talk->average_rating 
+                );
+            }
+        }
+        echo json_encode($returnArray);
+        break;
+}
--- hackathon/gitrace/getcommits.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$x = file_get_contents('https://api.github.com/repos/zfcampus/zf-apigility/commits?access_token=d3dd4be6a770aeb3f78d253b6096f36386fcead4');
-
-$data = json_decode($x);
-print count($data);
