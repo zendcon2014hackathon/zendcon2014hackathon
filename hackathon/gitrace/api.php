e54dcc15ada8641129bc3d72ba6dd90341211416
wimg <wim@wimgodden.be>
2014-10-31 00:26:10 -0700

    Speed updates

--- hackathon/gitrace/api.php
+++ hackathon/gitrace/api.php
@@ -1,5 +1,6 @@
 <?php
 header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
 
 function callAPI($function, $params, $querystring = '') {
     $paramString = '';
@@ -18,7 +19,6 @@ function callAPI($function, $params, $querystring = '') {
     $result = json_decode($result);
     return $result;    
 }
-
 
 switch ($_GET['action']) {
     case 'getevents':
@@ -57,35 +57,49 @@ switch ($_GET['action']) {
         foreach ($talks->talks as $talk) {
             if (isset($talk->speakers) && isset($talk->speakers[0]) && isset($talk->speakers[0]->speaker_uri) && $talk->average_rating > 3 && $talk->comment_count > 1) {
                 $speakers[array_pop(explode('/', $talk->speakers[0]->speaker_uri))] = $talk->speakers[0]->speaker_name;
-                $talklist[array_pop(explode('/', $talk->speakers[0]->speaker_uri))][] = array_pop(explode('/', $talk->uri));
+                $speakertalklist[array_pop(explode('/', $talk->speakers[0]->speaker_uri))][] = array_pop(explode('/', $talk->uri));
+                $talklist[array_pop(explode('/', $talk->uri))] = array(
+                    'comments' => 0,
+                    'totalRating' => 0,
+                    'lowestRating' => 5,
+                    'highestRating' => 0
+                );
             }
         }
-
+        $comments = callAPI('talk_comments', array('events' => $_GET['id']), 'resultsperpage=0');
+        foreach ($comments->comments as $comment) {
+            if (!is_null($comment->user_display_name) && $comment->rating > 0 && isset($talklist[array_pop(explode('/', $comment->talk_uri))])) {
+                $talklist[array_pop(explode('/', $comment->talk_uri))]['comments']++;
+                $talklist[array_pop(explode('/', $comment->talk_uri))]['totalRating'] += $comment->rating;
+                if ($comment->rating < $talklist[array_pop(explode('/', $comment->talk_uri))]['lowestRating']) {
+                    $talklist[array_pop(explode('/', $comment->talk_uri))]['lowestRating'] = $comment->rating; 
+                }
+                if ($comment->rating > $talklist[array_pop(explode('/', $comment->talk_uri))]['highestRating']) {
+                    $talklist[array_pop(explode('/', $comment->talk_uri))]['highestRating'] = $comment->rating;
+                }
+            }
+        }
+        
         $returnArray = array();
-        foreach ($talklist as $id => $talks) {
+        foreach ($speakertalklist as $id => $talks) {
             $rating = 0;
             $commentCount = 0;
             $lowestRating = 5;
             $highestRating = 0;
             for ($cnt = 0; $cnt < count($talks); $cnt++) {
-                $comments = callAPI('comments', array('talks' => $talks[$cnt]));
-                foreach ($comments->comments as $comment) {
-                    if ($comment->rating > 0 && isset($comment->user_uri)) { // Speaker comments don't count
-                        $rating += $comment->rating;
-                        if ($comment->rating < $lowestRating) {
-                            $lowestRating = $comment->rating;
-                        }
-                        if ($comment->rating > $highestRating) {
-                            $highestRating = $comment->rating;
-                        }
-                        $commentCount++;
-                    }
+                if ($talklist[$talks[$cnt]]['lowestRating'] < $lowestRating) {
+                    $lowestRating = $talklist[$talks[$cnt]]['lowestRating'];
                 }
+                if ($talklist[$talks[$cnt]]['highestRating'] > $highestRating) {
+                    $highestRating = $talklist[$talks[$cnt]]['highestRating'];
+                }
+                $rating += $talklist[$talks[$cnt]]['totalRating'];
+                $commentCount += $talklist[$talks[$cnt]]['comments'];
             };
-            
+
             $returnArray[] = array(
                 'name' => $speakers[$id],
-                'avg' => $rating / $commentCount,
+                'avg' => round($rating / $commentCount, 1),
                 'lowest' => $lowestRating,
                 'highest' => $highestRating
             );
